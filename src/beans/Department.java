3b50324ffc2c0fb4d84c8b8ec3a8d33dd884d69e
firoz <firozaalam99@gmail.com>
2020-03-13 21:53:56 +0530

    Create table
    Create table by hbm2ddl.auto

---
+++ src/beans/Department.java
@@ -0,0 +1,20 @@
+package beans;
+
+public class Department {
+	private int did;
+	private String dName;
+	public int getDid() {
+		return did;
+	}
+	public void setDid(int did) {
+		this.did = did;
+	}
+	public String getdName() {
+		return dName;
+	}
+	public void setdName(String dName) {
+		this.dName = dName;
+	}
+	
+
+}
---
+++ src/beans/Employee.java
@@ -0,0 +1,32 @@
+package beans;
+
+public class Employee {
+	private int id;
+	private String Name;
+	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
